597404cce672be52f6ef90e8ac8a552f0ddf5f16
Alex Hort-Francis <ajhf@hotmail.co.uk>
2025-04-02 22:46:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ot-outliers-variable-type-01
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        ot-outliers-variable-type-01
+      </div>
+    </>
+  )
+}
+
+export default App
